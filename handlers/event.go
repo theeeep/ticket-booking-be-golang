835859c167821bbe145e3860d4a9098cd391c146
Deepak <ideepakrabindra@gmail.com>
2025-01-15 23:07:07 +0530

    add fiber

--- handlers/event.go
+++ handlers/event.go
@@ -1,1 +1,19 @@
 package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/theeeep/ticket-booking-be/models"
+)
+
+type Eventhandler struct {
+	repository models.EventRepository
+}
+
+func NewEventhandler(router fiber.Router, repository models.EventRepository) {
+	handler := &Eventhandler{
+		repository: repository,
+	}
+	router.Get("/", handler.GetMany)
+	router.Post("/", handler.CreateOne)
+	router.Get("/:id", handler.GetOne)
+}
